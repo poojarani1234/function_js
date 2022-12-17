d508c1a741924da846856d8182779fdb58db5614
poojarani1234 <101449774+poojarani1234@users.noreply.github.com>
2022-12-17 10:16:24 +0530

    Add files via upload

---
+++ arrow_function.js
@@ -0,0 +1,37 @@
+
+
+var sum=(a)=>{(6);if(a%2==0);console.log(a,"even");console.log(a,"odd")}
+
+    
+// const squar=(x)=>{
+//     return x*x
+// }
+// let sum=(x,y)=>{
+//     return x+y
+// }
+// console.log(squar(4),sum(3,5))
+
+// const squar=(x)=>{return x*x};let sum=(x,y)=>{return x+y};console.log(squar(4),sum(3,5))
+
+
+// const divide=(a,b)=>a/b;console.log(divide(4,8));
+ 
+
+// const divide=(a,b)=>a**b;console.log(divide(2,3));
+
+
+// let fun= (a, b) => a * b;console.log(fun(5,4))
+
+
+// let Hello=()=>{return "Hello pujarani";};console.log(Hello())
+
+
+// let val=() => "Hello "+"val";console.log(val())
+
+// var sum = (a, b) => {
+//     let result = a + b;
+//     return result;
+// }
+
+// let result1 = sum(5,7);
+// console.log(result1); 
